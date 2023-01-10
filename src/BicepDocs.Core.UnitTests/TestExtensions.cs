f80cc0be0b4386d9c33425710590e0868dc21f35
Joachim Dalen <me@d3v.no>
2023-01-10 07:07:10 +0100

    fix line endings

--- src/BicepDocs.Core.UnitTests/TestExtensions.cs
+++ src/BicepDocs.Core.UnitTests/TestExtensions.cs
@@ -4,7 +4,11 @@ namespace LandingZones.Tools.BicepDocs.Core.UnitTests
 {
     public static class TestExtensions
     {
-        public static string ToPlatformLineEndings(this string text) => text.Replace("\r\n", Environment.NewLine).Replace("\n", Environment.NewLine).Replace("\r", Environment.NewLine);
+        public static string ToPlatformLineEndings(this string text)
+        {
+            return text.Contains("\r\n") ? text.Replace("\r\n", "\n") : text.Replace("\n", Environment.NewLine).Replace("\r", Environment.NewLine);
+        }
+
         public static string WithPlatformRootPath(this string text) => Path.Combine(Path.GetPathRoot(Environment.CurrentDirectory)!, text.TrimStart('/').TrimStart('\\').ToPlatformPath());
     }
 }
